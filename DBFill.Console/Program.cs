90f4d917c24f6931a2ec1c589dbe0ede32eabc9f
Egor Timashev <metallagetemp@gmail.com>
2019-08-20 17:24:44 +0300

    новый проект консольного интерфейса

---
+++ DBFill.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBFill.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
