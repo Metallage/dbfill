6989242c94c45784ebb06a440b1071dcd4da9ca5
Egor Timashev <metallagetemp@gmail.com>
2019-08-20 16:57:36 +0300

    Добавил коласс для Фамилий

---
+++ core/LastName.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DBFill.Core
+{
+    class LastName
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public LastName()
+        {
+
+        }
+    }
+}
